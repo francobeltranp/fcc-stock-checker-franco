b8f96ff2f8eaac835205206e8d784148a09f49f6
francobeltranp <123977145+francobeltranp@users.noreply.github.com>
2025-12-04 21:10:07 +0000

    feat: consulta básica de precio de stock

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,39 @@
 'use strict';
 
+const fetch = require('node-fetch');
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(async function (req, res){
+
+
+       const stock = req.query.stock;
+
+      if (!stock) {
+        return res.json({ error: 'Falta el parámetro stock' });
+      }
+
+      try {
+
+        const response = await fetch(
+          `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+        );
+
+        const data = await response.json();
+
+        return res.json({
+          stockData: {
+            stock: data.symbol,
+            price: data.latestPrice,
+            likes: 0
+          }
+        });
+
+      } catch (error) {
+        return res.json({ error: 'No se pudo obtener el precio' });
+      }
+      
       
     });
     
